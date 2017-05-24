e084cc4c87d4a6298cbd3152793af8e6baf7e8b0
ZiyueHuang <zyhuang94@gmail.com>
2017-05-24 19:14:57 +0800

    assignment 1 done.

---
+++ recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,50 @@
+package recfun
+import common._
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+    if(c == 0 || c == r) 1
+    else pascal(c-1,r-1) +  pascal(c,r-1)
+  }
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    
+    def remain_bracket(chars: List[Char], count: Int): Int = {
+      if(chars.isEmpty || count < 0){
+        count
+      } else{
+        if( chars.head == '(') remain_bracket(chars.tail, count + 1) 
+        else if (chars.head == ')') remain_bracket(chars.tail, count - 1)
+        else remain_bracket(chars.tail, count)
+      } 
+    }
+    
+    remain_bracket(chars, 0) == 0
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = {
+  
+    if (coins.isEmpty || money < 0) 0
+    else if (money == 0) 1
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    //whether to use the first denomination of coins
+  }
+}
